712937c82b4e84ff234afb28c06bf8dbfa14f6f8
Jiaao Hou <jiaao.hou@yale.edu>
2014-07-14 15:51:37 -0400

    starting navbar, finished editing profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,17 @@
 class UsersController < ApplicationController
 	def dashboard
-
 	end
+
+	def edit
+	end
+
+	def update
+		current_user.update_attributes(user_params)
+		redirect_to edit_user_path
+	end
+
+	private
+		def user_params
+			params.require(:user).permit(:email, :location, :gender)
+		end
 end
