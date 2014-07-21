cbb8775eff7247520aeca52b31d870353e45bf04
Sonja Peterson <sonja.peterson@gmail.com>
2014-07-21 15:38:17 -0400

    Tested some facebook integration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -35,6 +35,7 @@ class UsersController < ApplicationController
 
 
 
+
 	private
 		def user_params
 			params.require(:user).permit(:email, :location, :gender, :age, :fav_books, :fav_authors, :genre, :description)
