6a63b46f383569b26b5f16e6863e7f068f255696
E-Lynn Yap <yapelynn@gmail.com>
2014-07-24 13:42:13 -0400

    Finished restyling add a note

--- app/assets/javascripts/groups.js
+++ app/assets/javascripts/groups.js
@@ -27,3 +27,15 @@ $(document).ready(function () {
     });
 
 });
+
+$(".accordion dd").on("click", "a:eq(0)", function (event) {
+    var dd_parent = $(this).parent();
+
+    if(dd_parent.hasClass('active'))
+      $(".accordion dd div.content:visible").slideToggle("normal");
+    else {
+      $(".accordion dd div.content:visible").slideToggle("normal");
+      $(this).parent().find(".content").slideToggle("normal");
+    }
+
+});
